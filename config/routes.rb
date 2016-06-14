315670af62da10b90dc0633ccd31a441d0955c72
Sal Espinosa <sespinos@gmail.com>
2016-06-14 10:09:44 -0600

    4th fifteen

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :orders, only: [:index, :show]
   resources :users,  only: [:index, :show]
 
+  get '/search', to: "search#index"
+
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
       resources :items, except: [:new, :edit, :update]
